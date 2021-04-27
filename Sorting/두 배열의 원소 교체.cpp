198e3a7d373f29b130f07a4827d04ee141f41640
HYH <babypooh2001@naver.com>
2021-04-27 18:00:04 +0900

    두 배열의 원소 교체

--- Sorting/두 배열의 원소 교체.cpp
+++ Sorting/두 배열의 원소 교체.cpp
@@ -4,9 +4,24 @@
 
 using namespace std; 
 
+bool compare(int n1, int n2)
+{
+	if (n1 > n2)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
+}
+
 int main()
 {
 	int N, K;
+	int nValue = 0;
+
 	vector<int> arrA;
 	vector<int> arrB;
 
@@ -21,7 +36,7 @@ int main()
 		arrA.push_back(nTemp);
 	}
 
-	for (int i = 0; i < K; i++)
+	for (int i = 0; i < N; i++)
 	{
 		int nTemp = 0;
 
@@ -30,7 +45,23 @@ int main()
 		arrB.push_back(nTemp);
 	}
 
-	cout << "test";
+	sort(arrA.begin(), arrA.end());				// 배열A 오름차순 정렬
+	sort(arrB.begin(), arrB.end(), compare);	// 배열B 내림차순 정렬
+		
+	for (int i = 0; i < K; i++)
+	{
+		if (arrA[i] < arrB[i])
+		{
+			swap(arrA[i], arrB[i]);
+		}		
+	}
+
+	for (int i = 0; i < arrA.size(); i++)
+	{
+		nValue += arrA[i];
+	}
+
+	cout << nValue;
 
 	return 0;
 }
