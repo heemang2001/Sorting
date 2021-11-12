b225fc6dda6e9b2afe8879934c9810e43583fcae
heemang2001 <babypooh2001@naver.com>
2021-11-12 22:14:19 +0900

    좋디

---
+++ Sorting/좋다.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N;
+int Answer;
+
+int main()
+{
+
+
+	cout << Answer << '\n';
+
+	return 0;
+}
